6b3e2ac91c3d2fe50c442d5955affc1ef5a9063e
Fabricio <fabriciobencomo.viloria@gmail.com>
2025-05-05 02:37:43 -0400

    many changes

---
+++ src/products/products.service.ts
@@ -0,0 +1,84 @@
+import { BadRequestException, Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { read } from 'fs';
+import { Product } from './entities/product.entity';
+import {validate as isUUID} from 'uuid';
+import { Repository } from 'typeorm';
+import { PaginationDto } from '../common/dtos/pagination.dto';
+
+@Injectable()
+export class ProductsService {
+
+  private readonly logger = new Logger('ProductsService');
+
+  constructor(    
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>
+  ) 
+  {}
+
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save(product);
+      return product;
+    } catch (error) {
+      this.handleExeception(error);
+    }
+  }
+
+  async findAll(paginationDto: PaginationDto) {
+    const {limit = 10, offset = 0} = paginationDto;
+    try {
+      const products = await this.productRepository.find({
+        take: limit,
+        skip: offset,
+      });
+      return products
+    } catch (error) {
+      this.handleExeception(error);
+    }
+  }
+
+  async findOne(term: string) {
+    try {
+      let product: Product;
+      if(isUUID(term)) {
+        product = await this.productRepository.findOneBy({id: term}) as Product; 
+      }else {
+        // product = (await this.productRepository.findOneBy({slug: term})) as Product; 
+        const queryBuilder = this.productRepository.createQueryBuilder();
+        product = await queryBuilder.where('UPPER(title) =:title or slug =:slug', {
+          title: term.toUpperCase(),
+          slug: term.toLowerCase()
+        }).getOne() as Product;
+      }
+      if(!product) throw new NotFoundException(`Product with id ${term} not found`);
+      return product
+    } catch (error) {
+      this.handleExeception(error);
+    }
+  }
+
+  update(id: string, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: string) {
+    const product = this.findOne(id);
+    if(!product) throw new NotFoundException(`Product with id ${id} not found`);
+    this.productRepository.delete(id);
+    return product;
+  }
+
+  private handleExeception(error: any) {
+    if (error.code === '23505') {
+      this.logger.error('Product already exists', error);
+      throw new BadRequestException('Product already exists');
+    }
+    throw new Error(error);
+  }
+}
