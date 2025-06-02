e68be049a563e8c9734f53f133fbc44beddf0edf
Fabricio <fabriciobencomo.viloria@gmail.com>
2025-06-01 23:10:15 -0400

    added jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,12 +3,30 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
     imports:[
       TypeOrmModule.forFeature([User]),
+      PassportModule.register({
+        defaultStrategy: 'jwt',
+      }),
+      JwtModule.registerAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => {
+          return {
+            secret: configService.get('JWT_SECRET'),
+            signOptions: {
+              expiresIn: '1h', // Token expiration time
+            }  
+          }
+        }
+      }),
     ],
   exports: [AuthService, TypeOrmModule],
 })
